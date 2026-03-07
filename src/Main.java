4115e40614879c65ba7b6d393cec504e544880e2
Riley <riley.nelson.rw@gmail.com>
2026-03-07 03:35:12 -0500

    hope this fixes it

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Game game = new Game();
-
+        //can you see this
 
         boolean x = game.askPlayerName();
         game.startGame(x);
